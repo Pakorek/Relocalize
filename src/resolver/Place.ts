c722ae67bb322c08c0eef8da47210e9b4ffc572e
b.darmenia <bdarmenia@bluesoft-group.com>
2023-08-29 10:46:52 +0200

    rename createPlace method

--- src/resolver/Place.ts
+++ src/resolver/Place.ts
@@ -8,7 +8,7 @@ export class PlaceResolver {
 
   @Mutation(() => Place)
   @Authorized()
-  public async createOwnerPlace(
+  public async createPlace(
     @Arg('values', () => Place) values: Place,
     @Ctx() ctx
   ): Promise<Place | void> {
